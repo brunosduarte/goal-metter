9c37d34521523b214bb3579382557e4f20697e15
Bruno Duarte <brunosduarte@hotmail.com>
2024-09-11 00:42:21 -0300

    feat: pages implementation

--- src/components/empty-goals.tsx
+++ src/components/empty-goals.tsx
@@ -1,21 +1,22 @@
-import { Plus } from "lucide-react"
-import logo from '../assets/logo.svg'
-import letsStart from '../assets/illustration.svg'
-import { Button } from "./ui/button"
-import { DialogTrigger } from "./ui/dialog"
+import logo from '../assets/logo-in-orbit.svg'
+import letsStart from '../assets/lets-start-illustration.svg'
+
+import { DialogTrigger } from './ui/dialog'
+import { Button } from './ui/button'
+import { Plus } from 'lucide-react'
 
 export function EmptyGoals() {
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="logo" />
-      <img src={letsStart} alt="lets start" />
+      <img src={logo} alt="in.orbit" />
+      <img src={letsStart} alt="in.orbit" />
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora mesmo?
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
 
       <DialogTrigger asChild>
         <Button>
-          <Plus className="size-4"/>
+          <Plus className="size-4" />
           Cadastrar meta
         </Button>
       </DialogTrigger>
