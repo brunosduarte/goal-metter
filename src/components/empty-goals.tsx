6a9fecd10516a512264e73235a42cd992e88e12c
Bruno Duarte <brunosduarte@hotmail.com>
2024-09-13 03:09:09 -0300

    fix: rebranding issues

--- src/components/empty-goals.tsx
+++ src/components/empty-goals.tsx
@@ -1,6 +1,5 @@
-import logo from '../assets/logo.svg'
 import letsStart from '../assets/illustration.svg'
-
+import { GoalsKeeperIcon } from './icon'
 import { DialogTrigger } from './ui/dialog'
 import { Button } from './ui/button'
 import { Plus } from 'lucide-react'
@@ -8,10 +7,10 @@ import { Plus } from 'lucide-react'
 export function EmptyGoals() {
   return (
     <div className="h-screen flex flex-col items-center justify-center gap-8">
-      <img src={logo} alt="" />
+      <GoalsKeeperIcon />
       <img src={letsStart} alt="" />
       <p className="text-zinc-300 leading-relaxed max-w-80 text-center">
-        Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
+        Você ainda não cadastrou nenhuma meta, que tal cadastrar uma agora mesmo?
       </p>
 
       <DialogTrigger asChild>
